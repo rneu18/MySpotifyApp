8b9f80303d8f675f4e518c85a88cdcb257b5844e
rishikesh18 <rishi.neupane@gmail.com>
2019-03-12 11:59:26 -0400

    icon loaded

--- app/src/main/java/com/example/myspotifyapp/MainActivity.java
+++ app/src/main/java/com/example/myspotifyapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.myspotifyapp;
 
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
@@ -12,6 +13,12 @@ public class MainActivity extends AppCompatActivity {
     private TabAdapter adapter;
     private TabLayout tabLayout;
     private ViewPager viewPager;
+
+    public int[] icons= {
+            R.drawable.rock,
+            R.drawable.classic,
+            R.drawable.pop
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +33,15 @@ public class MainActivity extends AppCompatActivity {
         adapter.addFragment(new ClassicFragment(), "Classic");
         adapter.addFragment(new PopFragment(), "Pop");
         viewPager.setAdapter(adapter);
-        tabLayout.setupWithViewPager(viewPager);
+        tabLayout.setupWithViewPager(viewPager, false);
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+
+        for (int iconsLoad = 0; iconsLoad < tabLayout.getTabCount(); iconsLoad++) {
+            tabLayout.getTabAt(iconsLoad).setIcon(icons[iconsLoad]);
+        }
     }
 
 }
